a0dd9ef9ce3a340556e7ba2655dc5eecde1cd3ac
Raja s <rajaaltus@gmail.com>
2020-03-12 20:07:44 +0700

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container">
+    <h1 class="text-center">
+      Welcome
+    </h1>
+  </div>
+</template>
+<script>
+export default {
+  head() {
+    return {
+      title: 'Dokkoun Sole Co., Ltd - Vientiane, Laos'
+    }
+  }
+}
+</script>
